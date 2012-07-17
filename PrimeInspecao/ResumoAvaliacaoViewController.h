a0f3d68bac03cbcb3a1bed088fe86a867b5e78ac
ziglee <ziglee@gmail.com>
2012-07-16 22:36:42 -0300

    ...

---
+++ PrimeInspecao/ResumoAvaliacaoViewController.h
@@ -0,0 +1,26 @@
+//
+//  ResumoAvaliacaoViewController.h
+//  PrimeInspecao
+//
+//  Created by Cassio Ribeiro on 16/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Avaliacao.h"
+#import "Obra.h"
+#import "PerguntaRespostaCell.h"
+#import <UIKit/UIKit.h>
+
+@interface ResumoAvaliacaoViewController : UITableViewController
+
+@property (nonatomic, strong) NSDateFormatter *dateFormatter;
+@property (strong, nonatomic) Avaliacao *avaliacao;
+@property (strong, nonatomic) NSArray *secoesPerguntas;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (strong, nonatomic) IBOutlet UILabel *nomeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) IBOutlet UITextView *comentCriticosTextView;
+@property (strong, nonatomic) IBOutlet UITextView *comentMelhorarTextView;
+@property (strong, nonatomic) IBOutlet UITextView *comentPositivosTextView;
+
+@end
